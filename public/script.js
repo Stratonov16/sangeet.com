8a83d94d83627bbd936cb2c0fbb69bd96337000a
Glitch (hello-express) <none>
2021-05-06 00:06:13 +0000

    💽👛 Checkpoint ./views/index.html:31449278/464 ./public/script.js:31449278/799

--- public/script.js
+++ public/script.js
@@ -11,28 +11,4 @@ function search(data) {
   // TODO
 }
 
-// fetch the initial list of dreams
-fetch("/dreams")
-  .then(response => response.json()) // parse the JSON from the server
-  .then(dreams => {
-    // remove the loading text
-    dreamsList.firstElementChild.remove();
-  
-    // iterate through every dream and add it to our page
-    dreams.forEach(appendNewDream);
-  
-    // listen for the form to be submitted and add a new dream when it is
-    dreamsForm.addEventListener("submit", event => {
-      // stop our form submission from refreshing the page
-      event.preventDefault();
 
-      // get dream value and add it to the list
-      let newDream = dreamsForm.elements.dream.value;
-      dreams.push(newDream);
-      appendNewDream(newDream);
-
-      // reset form
-      dreamsForm.reset();
-      dreamsForm.elements.dream.focus();
-    });
-  });
