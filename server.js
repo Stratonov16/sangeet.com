ab910b20439b6ed23f4fccf361a07c3f74571ae7
Glitch (hello-express) <none>
2021-05-06 02:14:24 +0000

    🐦🎂 Checkpoint ./package.json:31449278/1334 ./server.js:31449278/1368

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
-// Route for handling Spotify login request
+// This route is hit when a use clicks 'Login' in the website
+// We redirect the user to the Spotify login in order to authenticate the user and obtain a grant code
 app.get('/spotify/login', function(req, res) {
   // define scopes from Spotify documentation
   var scopes = 'user-read-private user-read-email';
@@ -34,11 +35,31 @@ app.get('/spotify/login', function(req, res) {
     '&redirect_uri=' + encodeURIComponent(process.env.REDIRECT_URL));
 });
 
+// Spotify sends a GET request back to this REDIRECT_URL we specified with a grant code in the url query  
+// We will send this code back to Spotify to get an access token
 app.get('/spotify/callback', function(req, res) {
   const {code} = req.query
   console.log({code})
-  res.redirect('https://accounts.spotify.com/api/token' +
-    '?response_type=code' +
-    '&redirect_uri=' + encodeURIComponent(process.env.REDIRECT_URL));
+  let code = req.query.code || null
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(
+        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+      ).toString('base64'))
+    },
+    json: true
+  }
+  // axios is a javascript library for sending 
+  axios.post(authOptions, function(error, response, body) {
+    var access_token = body.access_token
+    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+    res.redirect(uri + '?access_token=' + access_token)
+  })
 
 });
