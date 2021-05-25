7b1bf909af17505f5c8381eb6e04edc0cac17145
Glitch (hello-express) <none>
2021-05-25 20:34:11 +0000

    🐚🦀 Checkpoint ./server.js:31449278/2080

--- server.js
+++ server.js
@@ -27,15 +27,15 @@ if (!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET || !SPOTIFY_REDIRECT_URI) {
 app.use(express.static("public"));
 
 // return the index.html file when a GET request is made to the root path "/"
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/public/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/login", (request, response) => {
+app.get("/login", (req, res) => {
   // get the current date in epoch time - we will use this as a unique key
   const state = new Date.toISOString();
   //set up the response to send a cookie to the browser
-  response.cookie(AUTH_STATE_KEY, state);
+  res.cookie(AUTH_STATE_KEY, state);
   // build authorization url 
   const authUrl = "https://accounts.spotify.com/authorize?" +
     querystring.stringify({
@@ -43,11 +43,74 @@ app.get("/login", (request, response) => {
       client_id: SPOTIFY_CLIENT_ID,
       scope: AUTH_SCOPE,
       redirect_uri: SPOTIFY_REDIRECT_URI,
-      state: state
+      state
     })
   // redirect user to authUrl so they can grant permission to access Spotify
-  response.redirect(authUrl);
+  res.redirect(authUrl);
 });
+
+app.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[AUTH_STATE_KEY] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(AUTH_STATE_KEY);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token', // Spotify access token url
+      data: {
+        code,
+        redirect_uri: SPOTIFY_REDIRECT_URI,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET).toString('base64'))
+      },
+      json: true
+    };
+
+    axios.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        axios.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
 
 // after our app has been set up above, start listening on a port provided by Glitch
 app.listen(process.env.PORT, () => {
