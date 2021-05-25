89ef8cc5635f98e63eec845e41250834fe7406b8
Glitch (hello-express) <none>
2021-05-25 20:22:10 +0000

    👋🌦 Checkpoint ./server.js:31449278/1583

--- server.js
+++ server.js
@@ -4,18 +4,22 @@
 // we've started you off with Express (https://expressjs.com/) and axios (https://www.npmjs.com/package/axios)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
+const querystring = require("querystring");
 const axios = require("axios");
 
 // initialize an instance of express called 'app' 
 const app = express();
 
-// get 
-const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID 
-const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET 
-const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI
+const AUTH_STATE_KEY = "spotify_auth_state";
+const AUTH_SCOPE = "user-read-private user-read-email";
+
+// get secret values
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
 
 if (!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET || !SPOTIFY_REDIRECT_URI) {
-  console.error('ERROR: Missing one or more critical Spotify environment variables. Check .env file')
+  console.error("ERROR: Missing one or more critical Spotify environment variables. Check .env file");
 }
 
 // make all the files in 'public' available
@@ -28,8 +32,21 @@ app.get("/", (request, response) => {
 });
 
 app.get("/login", (request, response) => {
-  // TODO
-  console.log(request);
+  // get the current date in epoch time - we will use this as a unique key
+  const state = new Date.toISOString();
+  //set up the response to send a cookie to the browser
+  response.cookie(AUTH_STATE_KEY, state);
+  // build authorization url 
+  const authUrl = "https://accounts.spotify.com/authorize?" +
+    querystring.stringify({
+      response_type: 'code',  // for Authorization Code grant type
+      client_id: SPOTIFY_CLIENT_ID,
+      scope: AUTH_SCOPE,
+      redirect_uri: SPOTIFY_REDIRECT_URI,
+      state: state
+    })
+  // redirect user to authUrl so they can grant permission to access Spotify
+  response.redirect(authUrl);
 });
 
 // after our app has been set up above, start listening on a port provided by Glitch
