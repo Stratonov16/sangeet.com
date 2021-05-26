409d17a1ad24e2b2cc2ed4f360aba62d9f3c8fa8
Glitch (hello-express) <none>
2021-05-26 00:53:10 +0000

    🌄🚓 Checkpoint ./public/script.js:31449278/708 ./public/index.html:31449278/388

--- public/script.js
+++ public/script.js
@@ -1,16 +1,25 @@
 // client-side js, loaded by index.html
 // run by the browser each time the page is loaded
 
+console.log(axios)
 console.log("hello world :o");
 
 // define variables that reference elements on our page
 const mainContent = document.getElementById("main-content");
 
-// a helper function that creates a list item for a given dream
-function submitForm(event) {
+// a helper function that handles form submission
+const submitForm = (event) => {
   event.preventDefault() // prevent page from refreshing 
-  const formData = new FormData(event.target);
-  console.log(formData)
+  const { elements } = event.target
+  
+  // get form values
+  const track = elements.track.value
+  const artist = elements.artist.value
+  
+  // send 
+  
+  
+  
   
   
 }
