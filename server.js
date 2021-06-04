1d930a96803ab6a83df7dea546e0a7f0795709e1
Glitch (hello-express) <none>
2021-06-04 22:16:16 +0000

    👲🛫 Checkpoint ./server.js:31449278/359

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/) and axios (https://www.npmjs.com/package/axios)
-// you can always use whatever libraries or frameworks you'd like through `package.json` > "Add package".
 const express = require("express");
 const axios = require("axios");
 
@@ -12,8 +7,6 @@ const app = express();
 // set up the app to parse JSON request bodies
 app.use(express.json());
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
 // return the index.html file when a GET request is made to the root path "/"
