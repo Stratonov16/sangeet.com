7a17d8f3ca3136f034a3b5192178504c744e4658
Glitch (hello-express) <none>
2022-03-31 10:42:09 +0000

    🍋🌲 Checkpoint ./public/index.html:43462486/474 ./public/style.css:43462486/4908 ./public/script.js:43462486/17

--- public/script.js
+++ public/script.js
@@ -1,9 +1,9 @@
 // client-side js, loaded by index.html. Run when web page is loaded
 
-// any console.log in this file will appear the browser console
-console.log("Hello from script.js!")
+// // any console.log in this file will appear the browser console
+// console.log("Hello from script.js!")
 
 
 const submitForm = (event)=>{
-  console.log(event);
+  console.log("jajn");
 };
