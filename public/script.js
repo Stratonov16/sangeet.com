7a5e02cd0137dad4abad979cd2aad0e64ce2bd21
Glitch (sangeet-com) <none>
2024-05-29 20:06:19 +0000

    📫🚓 Checkpoint ./public/index.html:43462486/23952 ./public/script.js:43462486/9934 ./public/style.css:43462486/8164

--- public/script.js
+++ public/script.js
@@ -1,13 +1,8 @@
-// client-side js, loaded by index.html. Run when web page is loaded
-
-// // any console.log in this file will appear the browser console
-// console.log("Hello from script.js!")
 const axios = window.axios;
 const handlebars = window.Handlebars;
 const output = document.getElementById("recommendation-output");
 
 const submitForm = async (event) => {
-  //to handle network issue while submitting use async
   event.preventDefault();
   const { elements } = event.target;
   const track = elements.track.value;
@@ -18,13 +13,33 @@ const submitForm = async (event) => {
     result = await axios.post("/recommendations", { track, artist });
   } catch (err) {
     let errMsg = "Something went wrong";
-    if (err.response.data.message) {
+    if (err.response && err.response.data && err.response.data.message) {
       errMsg = err.response.data.message;
     }
-    return alert(errMsg);
+    Toastify({
+      text: errMsg,
+      duration: 3000,
+      gravity: "top",
+      position: "right",
+      backgroundColor: "#ff0000",
+      stopOnFocus: true,
+    }).showToast();
+    return;
   }
 
   const recommendations = result.data.tracks;
+  if (!recommendations.length) {
+    Toastify({
+      text: "No recommendations found.",
+      duration: 3000,
+      gravity: "top",
+      position: "right",
+      backgroundColor: "#ff0000",
+      stopOnFocus: true,
+    }).showToast();
+    return;
+  }
+
   const topRecs = recommendations.slice(0, 20);
 
   console.log(topRecs);
@@ -35,12 +50,19 @@ const submitForm = async (event) => {
 };
 
 const templateRaw = `
-<p>Discover new tunes!<p>
-<h6>If you like "{{track}}", you'll love:</h6>
-<ul>
-  {{#each topRecs}}
-  <li><h6> <a href="{{external_urls.spotify}}">
-  <img src ="https://cdn.glitch.global/885771ac-3cfa-4c5a-bde7-235096795ebd/pixlr-bg-result.png?v=1650800025230" width ="20px">{{name}}</a></h6></li>
-  {{/each}}
-</ul>
+  <p>Discover new tunes!<p>
+  <h6>If you like "{{track}}", you'll love:</h6>
+  <ul>
+    {{#each topRecs}}
+    <li>
+      <h6>
+        <a href="{{external_urls.spotify}}" target="_blank">
+          <img src="{{album.images.2.url}}" width="20px">
+          {{name}}
+        </a>
+        <audio controls src="{{preview_url}}"></audio>
+      </h6>
+    </li>
+    {{/each}}
+  </ul>
 `;
